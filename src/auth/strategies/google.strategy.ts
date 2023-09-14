cf0f5bf7e3bdf09de9b33edf1d3523d0a36212c6
Roman_Fokin <roman.fokin@arbitas.com>
2023-09-15 01:15:19 +0300

    add env to gitignore

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -31,7 +31,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     done: VerifyCallback,
   ): Promise<any> {
     const { id, name, emails, photos } = profile;
-    console.log('=============== profile', profile);
 
     const user = {
       provider: 'google',
