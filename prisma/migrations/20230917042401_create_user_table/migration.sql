77605a6b51e25ef66ccba2c6bdafa9775be9a5f9
Roman_Fokin <roman.fokin@arbitas.com>
2023-09-17 07:26:37 +0300

    add more data to user table

--- prisma/migrations/20230917042401_create_user_table/migration.sql
+++ prisma/migrations/20230917042401_create_user_table/migration.sql
@@ -1,11 +1,15 @@
 -- CreateTable
 CREATE TABLE `users` (
-    `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(255) NULL,
-    `username` VARCHAR(255) NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `first_name` VARCHAR(255) NOT NULL,
+    `last_name` VARCHAR(255) NULL,
+    `photo` TEXT NULL,
+    `username` VARCHAR(255) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_username_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
